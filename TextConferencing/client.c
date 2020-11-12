52381496e553482c3ba4848dcbea0b9b8a203c6b
Isamu <isamu.poy@mail.utoronto.ca>
2020-11-12 15:52:48 +0900

    added login and logout

--- TextConferencing/client.c
+++ TextConferencing/client.c
@@ -19,6 +19,7 @@
 #define NACK "NACK"
 #define ATTEMPT_LIMIT 50 //the round trip we have is about 70 usec, want the server to wait for about 3 msec to close, therefore chose 40 resend attempts
 
+bool joined = false;
 
 
 void chat(int sockfd) 
@@ -47,6 +48,8 @@ void login(char *cmd, int *sockfd){
     struct addrinfo hints, *servinfo, *p;
     char s[INET6_ADDRSTRLEN];
     int numbytes;
+    char *msgString;
+    char buffer[MAXBUFLEN];
     // extraect the above components from cmd
 
     cmd = strtok(NULL, " ");
@@ -65,13 +68,14 @@ void login(char *cmd, int *sockfd){
 		fprintf(stdout, "login format: /login <client_id> <password> <server_ip> <server_port>\n");
 		return 1;
 
-	}  else if(*socket != INVALID){
+	}  else if(*socket != INVALID_SOCKET){
         fprintf("ERROR: attempted multiple logins")
         return 1;
 
     } else {
         //instantiate TCP protocol
         int dummy;
+        memset(&hints, 0, sizeof hints);
         hints.ai_family = AF_UNSPEC;
         hints.ai_socktype = SOCK_STREAM; // TCP socket type
 
@@ -83,7 +87,7 @@ void login(char *cmd, int *sockfd){
         
         // referenced from Beej's code pg.35
         // get socket from server port
-        for(char *p = servinfo; p != NULL; p = p->ai_next) {
+        for(p = servinfo; p != NULL; p = p->ai_next) {
             if ((sockfd = socket(p->ai_family, p->ai_socktype,
                 p->ai_protocol)) == -1) {
                 perror("client: socket\n");
@@ -111,16 +115,73 @@ void login(char *cmd, int *sockfd){
         printf("client: connecting to %s\n", s);
         freeaddrinfo(servinfo); // all done with this structure
 
-        // TODO: implement login information processing
-        // TODO: modify packet.h to support different functions
         int numbytes;
-        struct packet packetto;
-        packetto.type = LOGIN;
+        struct message msg;
 
+        msg.type = LOGIN;
+        strncpy(msg.source, id, MAX_NAME);
+        strncpy(msg.data, password, MAX_DATA);
+        msg.size = strlen(msg.data);
+        msgString = formatMessage(msg, buffer);
+
+        if((numbytes = send(*sockfd, buf, MAXBUFLEN - 1, 0)) == -1){
+            fprintf(stderr, "login error\n");
+			close(*socketfd_p);
+            *sockfd = INVALID_SOCKET;
+			return;
+        }
         
+        // MESSAGE TYPES: 
+        // --> LO_ACK - login successful
+        // --> LO_NAK - login unsuccessful
+        buffer[numbytes] = 0;
+        if(msg.type = LO_ACK && pthread_create(receive_thread_p, NULL, receive, socketfd_p) == 0)){
+            fprintf(stdout, "login success!\n");
 
+        } else if (msg.type == LO_NAK) {
+            fprintf(stdout, "login failure b/c %s\n", msg.data);
+            close(*sockfd);
+            *sockfd = INVALID_SOCKET;
+
+			return;
+        } else {
+            fprintf(stdout, "INVALID INPUT: type %d, data %s\n", msg.type, msg.data);
+            close(*sockfd_p);
+            *sockfd_p = INVALID_SOCKET;
+            return;
+        } 
+    }
+}
+
+void logout(int *sockfd, pthread_t *rcv_thread){
+    if(*sockfd == INVALID_SOCKET) {
+        fprintf(stdout, "Currently no logins found.\n");
+        return;
     }
 
+    char buffer[MAXBUFLEN]; 
+    char* logmsg;
+    int numbytes;
+    struct message msg;
+    msg.type = EXIT;
+    msg.size = 0;
+
+    logmsg = formatMessage(msg, buffer);
+
+    if((numbytes = send(*sockfd, buffer, MAXBUFLEN - 1, 0)) == -1){
+        fprintf(stderr, "logout error\n");
+        return;
+    }
+
+    if (pthread_cancel(*rcv_thread)) {
+        fprintf(stderr, "logout failed\n");
+	} else {
+        fprintf(stdout, "logout success!\n");	
+	} 
+
+    joined = false;
+    close(*sockfd);
+    *sockfd = INVALID_SOCKET;
 
 }
 
@@ -128,18 +189,22 @@ void joinsession(char *session, int sockfd) {
     if (session == NULL) {
         fprintf(stdout, "invalid session id, input format: /joinsession <session ID>\n");
         return;
-    }
-    else if (sockfd == -1){
+    } else if (sockfd == -1){
         fprintf(stdout, "Please login to a server before trying to join a session\n");
         return;
-    }
-    else {
+
+    } else if(joined){
+        fprintf(stdout, "can't join multiple sessions")
+        return;
+
+    } else {
         struct message newMessage;
         newMessage.type = JOIN;
         newMessage.size = strlen(session);
         strcpy(newMessage.data, session, MAX_DATA);
         int bytes;
         char *buf;
+
         formatMessage(newMessage, buf);
         if ((bytes = send(sockfd, buf, MAXBUFLEN, 0)) == -1) {
             fprintf(stdout, "ERROR: send() failed\n");
@@ -153,8 +218,7 @@ void leavesession(int sockfd) {
     if (sockfd == -1) {
         fprintf(stdout, "Please login to a server before trying to join a session\n");
         return;
-    }
-    else {
+    } else {
         struct message newMessage;
         newMessage.type = LEAVE_SESS;
         newMessage.size = 0;
@@ -162,6 +226,7 @@ void leavesession(int sockfd) {
         int bytes;
         char *buf;
         formatMessage(newMessage, buf);
+
         if ((bytes = send(sockfd, buf, MAXBUFLEN, 0)) == -1) {
             fprintf(stdout, "ERROR: send() failed\n");
             return;
@@ -174,12 +239,10 @@ void createsession(char *session, int sockfd) {
     if (session == NULL) {
         fprintf(stdout, "invalid session id, input format: /createsession <session ID>\n");
         return;
-    }
-    else if (sockfd == -1) {
+    } else if (sockfd == INVALID_SOCKET) {
         fprintf(stdout, "Please login to a server before trying to join a session\n");
         return;
-    }
-    else {
+    } else {
         struct message newMessage;
         newMessage.type = NEW_SESS;
         newMessage.size = 0;
@@ -199,18 +262,19 @@ void list(int sockfd) {
     if (sockfd == -1) {
         fprintf(stdout, "Please login to a server before trying to join a session\n");
         return;
-    }
-    else {
+    } else {
         struct message newMessage;
         newMessage.type = QUERY;
         newMessage.size = 0;
         int bytes;
         char *buf;
         formatMessage(newMessage, buf);
+
         if ((bytes = send(sockfd, buf, MAXBUFLEN, 0)) == -1) {
             fprintf(stdout, "ERROR: send() failed\n");
             return;
         }
+
         return;
     }
 }
@@ -220,8 +284,8 @@ void list(int sockfd) {
 int main(int argc, char **argv){
     char *cmd; // will store a line of strings separated by spaces
     char *buff[MAXBUFLEN]; 
-    int socketfd = INVALID_SOCKET; // init socket value
-    pthread_t received_msg;
+    int sockfd = INVALID_SOCKET; // init socket value
+    pthread_t rcv_msg;
     
     // for(;;) is an infinite loop for C like while(1)
     for (;;) { 
@@ -243,28 +307,35 @@ int main(int argc, char **argv){
         if (strcmp(cmd, "/login") == 0) {
             // log into the server at a given address and port
             // the IP address is specified in string dot format
-			// loginServer();
+			login(cmd, sockfd);
+
 		} else if (strcmp(cmd, "/logout") == 0) {
             // exit the server
-			// logoutServer();
+			logout(sockfd, rcv_msg);
+
 		} else if (strcmp(cmd, "/joinsession") == 0) {
             // join the conference session with the given session id
             cmd = strtok(NULL, " "); //cmd should contain the session id
 			joinsession();
+
 		} else if (strcmp(cmd, "/leavesession") == 0) {
             // leave the currently established session
             joinsession();
+
 		} else if (strcmp(cmd, "/createsession") == 0) {
             // create a new conference session and join it
 			cmd = strtok(NULL, " "); //cmd should contain the session id
             joinsession();
+
 		} else if (strcmp(cmd, "/list") == 0) {
             // get the list of the connected clients and available sessions
 			// list();
+
 		} else if (strcmp(cmd, "/quit") == 0) {
             // terminate the program
-			// logout();
+			logout(sockfd, rcv_msg);
 			break;
+
 		} else{
             // send a message to the current conference session. The message
             // is sent after the new line
