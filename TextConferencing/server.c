fe72dfeae592d781f6b4eb88cebef78a4f0fa8ca
Isamu <isamu.poy@mail.utoronto.ca>
2020-11-29 11:18:50 +0900

    fixing list functoin

--- TextConferencing/server.c
+++ TextConferencing/server.c
@@ -399,7 +399,7 @@ void *message_handler(void *arg) {
                 struct user *people = ptr->users;
                 while(people->next != NULL){
                     if(people->name != NULL){
-                        strcat(people->name);
+                        strcat(data, people->name,);
                         strcat(data, ", ");
                     }
                     people = people->next;
